36e14da1eae5dd11cc9bbc02f7a0af82a6cafe29
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-02-17 16:03:28 -0300

    feat(CategoryCard.vue): replace Tag component with SelectableIngredient component in CategoryCard feat(SelectableIngredient.vue): create SelectableIngredient component to allow ingredient selection

---
+++ src/components/SelectableIngredient.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+
+export default {
+    name: "SelectableIngredient",
+    props: {
+        ingredient: { type: String, required: true }
+    },
+    data() {
+        return {
+            selected: false
+        }
+    },
+    methods: {
+        toggleSelected() {
+            this.selected = !this.selected;
+        }
+    },
+    components: { Tag }
+}
+</script>
+
+<template>
+    <button 
+        class="ingredient"
+        @click="toggleSelected()"
+    >
+        <Tag :text="ingredient" :active="selected" />
+    </button>
+</template>
+
+<style scoped>
+.ingredient {
+    cursor: pointer;
+}
+</style>
