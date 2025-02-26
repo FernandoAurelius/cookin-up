1ef50af5740a85e8dac83f663b72633d9a2962de
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-02-26 18:03:12 -0300

    feat: add .env file to .gitignore
    feat: add dotenv package
    
    feat(ShowRecipes.vue): add debug logging to improve component debugging
    
    chore(ShowRecipes.vue): refactor recipe comparison logic for better readability
    
    chore(ShowRecipes.vue): use import.meta.env.VITE_DEBUG for conditional debugging
    
    feat(ShowRecipes.vue): add watch for store.state.recipes to update recipes dynamically

--- src/components/ShowRecipes.vue
+++ src/components/ShowRecipes.vue
@@ -2,27 +2,48 @@
 import type IRecipe from '@/interfaces/IRecipe';
 import RecipeCard from './RecipeCard.vue';
 import { defineComponent } from 'vue';
+import.meta.env.VITE_BEBUG;
 
 export default defineComponent({
     name: "ShowRecipes",
     data() {
         return {
+            DEBUG: import.meta.env.VITE_DEBUG === 'true',
             recipes: [] as IRecipe[],
             ingredients: this.$store.state.ingredients as string[]
         }
     },
     methods: {
         compareRecipes(recipeIngredients: string[], userIngredients: string[]) {
-            return recipeIngredients.every((ingredient: string) => {
+            if (this.DEBUG) console.log('Comparing recipes:', { recipeIngredients, userIngredients });
+            const result = recipeIngredients.every((ingredient: string) => {
+                if (this.DEBUG) console.log(`Checking if ${ingredient} is in user ingredients:`, userIngredients.includes(ingredient));
                 return userIngredients.includes(ingredient);
             });
+            if (this.DEBUG) console.log('Recipe match result:', result);
+            return result;
         }
     },
     created() {
+        if (this.DEBUG) console.log('ShowRecipes component created');
+        if (this.DEBUG) console.log('Dispatching loadRecipes action');
         this.$store.dispatch("loadRecipes");
-        this.recipes = this.$store.state.recipes.filter((recipe: IRecipe) => {
-            return this.compareRecipes(recipe.ingredients, this.ingredients);
-        });
+        if (this.DEBUG) console.log('Store recipes:', this.$store.state.recipes);
+        if (this.DEBUG) console.log('User ingredients:', this.ingredients);
+    },
+    watch: {
+        "$store.state.recipes": {
+            handler(newRecipes: IRecipe[]) {
+                if (this.DEBUG) console.log('Recipes updated in store:', newRecipes);
+                if (this.DEBUG) console.log('User ingredients:', this.ingredients);
+                this.recipes = newRecipes.filter((recipe: IRecipe) => {
+                    if (this.DEBUG) console.log(`Checking recipe ${recipe.name} with ingredients:`, recipe.ingredients);
+                    return this.compareRecipes(recipe.ingredients, this.ingredients);
+                });
+                if (this.DEBUG) console.log('Filtered recipes:', this.recipes);
+            },
+            deep: true
+        }
     },
     components: { RecipeCard }
 })
